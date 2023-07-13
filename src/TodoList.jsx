2d6e952fe750efef94eccebec3937a157ceee0fb
athuljain <athuljainkj2@gmail.com>
2023-07-13 10:24:06 +0530

    Added css

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,45 +1,98 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
-import { addTodo, deleteTodo } from "./todoListSlice";
-import "./TodoList.css"
+import { addTodo, deleteTodo, editTodo } from "./todoListSlice";
+import "./TodoList.css";
 
 const TodoList = () => {
-  const [newTodo, setNewTodo] = useState(""); 
+  const [newTodo, setNewTodo] = useState("");
+  const [editingText, setEditingText] = useState("");
+  const [editingTodoId, setEditingTodoId] = useState(null);
   const todos = useSelector((state) => state.todoList);
   const dispatch = useDispatch();
 
   const handleAddTodo = () => {
-    dispatch(addTodo({ id: Date.now(), text: newTodo })); 
-    setNewTodo(""); 
+    if (newTodo.trim() !== "") {
+      dispatch(addTodo({ id: Date.now(), text: newTodo }));
+      setNewTodo("");
+    }
   };
 
   const handleDeleteTodo = (id) => {
     dispatch(deleteTodo(id));
   };
 
+  const handleEditTodo = (id, newText) => {
+    dispatch(editTodo({ id, text: newText }));
+    setEditingText("");
+    setEditingTodoId(null);
+  };
+  console.log("kvm", setEditingText);
+  console.log(setEditingTodoId);
+
   return (
     <div className="todo-inputcontainer">
-        <div className="todo-input-feild" >
-        <input 
-        className="todo-input"
-        type="text"
-        placeholder="Add Todo"
-        value={newTodo} 
-        onChange={(e) => setNewTodo(e.target.value)} 
-      />
-      <button className="add-btn" onClick={handleAddTodo}>Add</button> {}
+      <div className="todo-input-feild">
+        <input
+          className="todo-input"
+          type="text"
+          placeholder="Add Todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button className="add-btn" onClick={handleAddTodo}>
+          Add
+        </button>
       </div>
       <div className="todo-list">
-      <ul className="todo-ul">
-        {todos.map((todo) => (
-          <li className="todo-li" key={todo.id}>
-            {todo.text}
-            <button className="todo-delete-btn" onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+        <ul className="todo-ul">
+          {todos.map((todo) => (
+            <li className="todo-li" key={todo.id}>
+              {editingTodoId === todo.id ? (
+                <div>
+                  <input className="input-edit"
+                    type="text"
+                    value={editingText}
+                    onChange={(e) => setEditingText(e.target.value)}
+                  />
+                  <button
+                    className="todo-save-btn"
+                    onClick={() => {
+                      handleEditTodo(todo.id, editingText);
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              ) : (
+                <div>
+                  {todo.id === editingTodoId ? (
+                    <div>{editingText}</div>
+                  ) : (
+                    <div>{todo.text}</div>
+                  )}
+                  <div>
+                  <button
+                    className="todo-edit-btn"
+                    onClick={() => {
+                      setEditingText(todo.text);
+                      setEditingTodoId(todo.id);
+                    }}
+                  >
+                    Edit
+                  </button>
+                  </div>
+                </div>
+              )}
+              <button
+                className="todo-delete-btn"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-      
     </div>
   );
 };
--- src/todoListSlice.jsx
+++ src/todoListSlice.jsx
@@ -10,8 +10,15 @@ const todoListSlice = createSlice({
     deleteTodo: (state, action) => {
       return state.filter((todo) => todo.id !== action.payload);
     },
+    editTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todoIndex = state.findIndex((todo) => todo.id === id);
+      if (todoIndex !== -1) {
+        state[todoIndex].text = text;
+      }
+    },
   },
 });
 
-export const { addTodo, deleteTodo } = todoListSlice.actions;
+export const { addTodo, deleteTodo, editTodo } = todoListSlice.actions;
 export default todoListSlice.reducer;
